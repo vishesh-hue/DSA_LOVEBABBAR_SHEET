ea022ba67be156b396a9c86dffa04c6d13edae08
vishesh tiwari <tvishesh593@gmail.com>
2022-10-29 17:25:12 +0530

    Create arrays-3.java

---
+++ arrays-3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ 
+ 
+ public class minmax{
+     
+     public static void main(string[] args){
+         int[] arr= {6,2,3,4,5,9}
+         int k=3;
+         Arrays.sort(arr);
+         System.out.println(arr(k-1));
+     }
+ }
